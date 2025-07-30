e4b66af9be1b265a92d94e31f8eb8d729b968546
RzlAm <112256482+RzlAm@users.noreply.github.com>
2025-07-30 12:15:40 +0700

    feat: add PWA support

--- app/Providers/Filament/AppPanelProvider.php
+++ app/Providers/Filament/AppPanelProvider.php
@@ -10,11 +10,13 @@ use Filament\Pages;
 use Filament\Panel;
 use Filament\PanelProvider;
 use Filament\Support\Colors\Color;
+use Filament\View\PanelsRenderHook;
 use Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse;
 use Illuminate\Cookie\Middleware\EncryptCookies;
 use Illuminate\Foundation\Http\Middleware\VerifyCsrfToken;
 use Illuminate\Routing\Middleware\SubstituteBindings;
 use Illuminate\Session\Middleware\StartSession;
+use Illuminate\Support\Facades\Blade;
 use Illuminate\View\Middleware\ShareErrorsFromSession;
 
 class AppPanelProvider extends PanelProvider
@@ -52,6 +54,11 @@ class AppPanelProvider extends PanelProvider
             ])
             ->authMiddleware([
                 Authenticate::class,
-            ]);
+            ])->renderHook(
+                PanelsRenderHook::HEAD_END,
+                function (): string {
+                    return Blade::render('@laravelPWA');
+                }
+            );
     }
 }
---
+++ config/laravelpwa.php
@@ -0,0 +1,50 @@
+<?php
+
+return [
+    'name' => 'LaravelPWA',
+    'manifest' => [
+        'name' => env('APP_NAME', 'My PWA App'),
+        'short_name' => 'PWA',
+        'start_url' => '/',
+        'background_color' => '#09090B',
+        'theme_color' => '#09090B',
+        'display' => 'standalone',
+        'orientation' => 'any',
+        'status_bar' => '#0B0B0D',
+        'icons' => [
+            '180x180' => [
+                'path' => '/apple-touch-icon.png',
+                'purpose' => 'any'
+            ],
+            '96x96' => [
+                'path' => '/favicon-96x96.png',
+                'purpose' => 'any'
+            ],
+            '192x192' => [
+                'path' => '/web-app-manifest-192x192.png',
+                'purpose' => 'any'
+            ],
+            '512x512' => [
+                'path' => '/web-app-manifest-512x512.png',
+                'purpose' => 'any'
+            ],
+        ],
+        'shortcuts' => [
+            [
+                'name' => 'Shortcut Link 1',
+                'description' => 'Shortcut Link 1 Description',
+                'url' => '/shortcutlink1',
+                'icons' => [
+                    "src" => "/ifavicon-96x96.png",
+                    "purpose" => "any"
+                ]
+            ],
+            [
+                'name' => 'Shortcut Link 2',
+                'description' => 'Shortcut Link 2 Description',
+                'url' => '/shortcutlink2'
+            ]
+        ],
+        'custom' => []
+    ]
+];
