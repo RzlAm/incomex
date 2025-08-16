b397cf4a97e61dad1db0199181126167c2aab51b
RzlAm <112256482+RzlAm@users.noreply.github.com>
2025-08-16 14:48:03 +0700

    refactor: redesign settings page

---
+++ app/Filament/Pages/Settings.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Filament\Pages;
+
+use Filament\Pages\Page;
+use App\Models\Setting;
+use Filament\Notifications\Notification;
+
+class Settings extends Page
+{
+    public $currency;
+    public $timezone;
+    public $timezones = [];
+
+    protected static ?string $navigationIcon = 'heroicon-o-cog-6-tooth';
+    protected static string $view = 'filament.pages.settings';
+    protected static ?int $navigationSort = 5;
+    protected static ?string $navigationGroup = 'Settings';
+
+    public function mount()
+    {
+        $setting = Setting::first();
+        $this->currency = $setting?->currency;
+        $this->timezone = $setting?->timezone;
+
+        $this->timezones = collect(timezone_identifiers_list())
+            ->mapWithKeys(fn($tz) => [$tz => $tz])
+            ->toArray();
+    }
+
+    public function submit()
+    {
+        $setting = Setting::first() ?? new Setting();
+        $setting->currency = $this->currency;
+        $setting->timezone = $this->timezone;
+        $setting->save();
+
+        Notification::make()
+            ->title('Settings updated successfully!')
+            ->success()
+            ->send();
+    }
+}
